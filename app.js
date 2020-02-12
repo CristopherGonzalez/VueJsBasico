9414d8d9c716535b519993d3b9f30f3a6aee1a60
Cristopher Gonzalez <cristopher.gonzalez@saludaysen.cl>
2020-02-12 10:54:17 -0300

    Se agregan directiva v-for para imprimir un objeto js, se define un index y una key para ver su uso

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ new Vue({
     return {
       name:'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent : 100
+      changePercent : 100,
+      pricesWithDays:[
+        {day : "Lunes", value: 8400 },
+        {day : "Martes", value: 7400 },
+        {day : "Miercoles", value: 8200 },
+        {day : "Jueves", value: 7400 },
+        {day : "Viernes", value: 9000 },
+        {day : "Sabado", value: 8300 },
+        {day : "Domingo", value: 8500 }
+      ]
     }
   },
 });
