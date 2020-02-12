9a62796fd4731fdaed323cf4ccf9268db7324fd2
Cristopher Gonzalez <cristopher.gonzalez@saludaysen.cl>
2020-02-12 12:07:46 -0300

    Se utilizan computer properties para actualizar el titulo y watchers para ver en consola cambio de estado de visibilidad, ambos en tiempo real

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 new Vue({
-  el:'#app',
+  el: '#app',
   data() {
     return {
-      name:'Bitcoin',
+      name: 'Bitcoin',
+      symbol: 'BTC',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      changePercent : -1,
-      pricesWithDays:[
-        {day : "Lunes", value: 8400 },
-        {day : "Martes", value: 7400 },
-        {day : "Miercoles", value: 8500 },
-        {day : "Jueves", value: 7900 },
-        {day : "Viernes", value: 9000 },
-        {day : "Sabado", value: 8000 },
-        {day : "Domingo", value: 9600 }
+      changePercent: -1,
+      pricesWithDays: [
+        { day: "Lunes", value: 8400 },
+        { day: "Martes", value: 7400 },
+        { day: "Miercoles", value: 8500 },
+        { day: "Jueves", value: 7900 },
+        { day: "Viernes", value: 9000 },
+        { day: "Sabado", value: 8000 },
+        { day: "Domingo", value: 9600 }
       ],
-      showPrices : false,
-      actualPrice : 8500,
+      showPrices: false,
+      actualPrice: 8500,
       color: 'f4f4f4'
     }
   },
   methods: {
-    toggleShowPrices(){
+    toggleShowPrices() {
       this.showPrices = !this.showPrices;
       this.color = this.color.split('').reverse().join('');
+    }
+  },
+  computed: {
+    title(){
+      return `${this.name} - ${this.symbol}`
+    }
+  },
+  watch: {
+    showPrices(newValue, oldValue){
+      console.log('new', newValue);
+      console.log('old', oldValue);
     }
   },
 });
